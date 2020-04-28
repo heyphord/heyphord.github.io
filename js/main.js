1144f31a3fdb93056a50907d649a935c4c70f79b
heyphord <heyphord@gmail.com>
2020-04-28 21:27:40 +0000

    added animations and scroll spy

--- js/main.js
+++ js/main.js
@@ -18,3 +18,26 @@
   
   });
 
+
+  $("#sendMail").submit(function (e) { 
+	  e.preventDefault();
+	  
+	  let name = $("#name").val();
+	  let subject = $("#subject").val();
+	  let message = $("#message").val();
+
+	  
+	  let link = `mailto:janiehaylimited@gmail.com?subject=${subject}&body=${message}%0D%0A%0D%0ASent by: ${name}`
+	  window.open(
+		link,
+		'_blank' // <- This is what makes it open in a new window.
+	  );
+	  
+	
+	  
+  });
+
+  var scroll = new SmoothScroll('a[href*="#"]');
+
+  //init wow
+  var WOW =new WOW().init();
