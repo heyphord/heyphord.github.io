5030e08f2b93194748b362759527ad6794dcf137
heyphord <heyphord@gmail.com>
2020-05-08 11:50:08 +0000

    carousel images added

--- js/main.js
+++ js/main.js
@@ -18,14 +18,7 @@
 		arrows: false,
 		autoplay: true,
 		responsive: [
-			{
-			  breakpoint: 1024,
-			  settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
-				
-			  }
-			},
+			
 			{
 				breakpoint: 780,
 				settings: {
